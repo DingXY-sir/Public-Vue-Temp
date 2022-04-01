565821dd3c1ee8f95fbe80e6268124341debebb5
丁心雨 <932978673@qq.com>
2022-04-01 10:42:14 +0800

    vite项目配置

---
+++ src/view/layout/layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout-container">
+    <div class="layout-header"><p>你好，vue3+TS！</p></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, computed, watch } from "vue";
+</script>
+<style lang="scss" scoped>
+.layout-header {
+  p {
+    font-size: 40px;
+    color: red;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //设置默认端口和自动打开浏览器
   server: {
-    port:9999
+    port: 9999,
+    open:true
   },
+  //全局引入预处理器sass
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:`@import "./src/styles/global.scss";` 
+      }
+    }
+  },
+  //设置别名
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
