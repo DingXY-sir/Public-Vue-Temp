6617668c0ffe5ddc089c38a11eceaff26e7f7ae3
丁心雨 <932978673@qq.com>
2022-04-02 15:54:00 +0800

    全局重制样式

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/index"
-import '@/styles/index.scss'
+import './styles/reset.scss'
 
 const app = createApp(App)
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
@@ -29,6 +30,9 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
     Components({
        // ui库解析器
       resolvers: [
